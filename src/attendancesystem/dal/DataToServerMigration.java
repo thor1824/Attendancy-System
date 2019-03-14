222ba16cbdaf3753a6e44966a5efab293ef3f642
thor1824 <42994659+thor1824@users.noreply.github.com>
2019-03-14 18:08:35 +0100

    Create DataToServerMigration.java

---
+++ src/attendancesystem/dal/DataToServerMigration.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendancesystem.dal;
+
+/**
+ *
+ * @author Thorbjørn Schultz Damkjær
+ */
+public class DataToServerMigration
+{
+    
+}
