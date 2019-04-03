1f08e2a90ea4ce7834e828c2a15ff1c6dbffa2aa
nijashansen <nijas.w.hansen@gmail.com>
2019-04-03 14:16:32 +0200

    Still working on it

--- src/attendancesystem/bll/LoginSimolator.java
+++ src/attendancesystem/bll/LoginSimolator.java
@@ -6,43 +6,47 @@
 package attendancesystem.bll;
 
 import attendancesystem.be.Student;
+import attendancesystem.be.User;
 import attendancesystem.dal.db.StudentDbDao;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
-
 
 /**
  *
  * @author Christian
  */
 public class LoginSimolator {
-    public StudentDbDao studentDb;
-    
-    
-    public List addStudentToList() throws SQLException, SQLServerException, IOException{
-      List<Student> simStudents = studentDb.getAllStudents().subList(0, 4);
-      
-      return simStudents;
-    }
-    
-    
-   
 
-       
-    
-            
-    
-   
-    public String getDate(){
-    DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-    Date date = new Date();
-    
-    return dateFormat.format(date);
+    public StudentDbDao studentDb;
+    List<Student> simStudents;
+
+    public List addStudentToList() throws SQLException, SQLServerException, IOException {
+
+        simStudents = studentDb.getAllStudents().subList(0, 4);
+
+        return simStudents;
+    }
+
+    public User setUserByTime() {
+        //todo
+
+        LocalDate locDate = LocalDate.now();
+
+        simStudents.get(0);
+
+        return null;
+    }
+
+    public String getDate() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+
+        return dateFormat.format(date);
     }
 }
